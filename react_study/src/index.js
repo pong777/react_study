174a763e2cf6d5901e873ba7b56c6a9774699251
주지원 <joo5909@interpark.com>
2021-06-28 17:31:31 +0900

    테스트 푸쉬

--- react_study/src/index.js
+++ react_study/src/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
